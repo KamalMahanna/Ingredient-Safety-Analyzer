9d2b252e316370e2483db82f1bcd63c2943c3871
KamalMahanna <kamalkiranmahanna@gmail.com>
2025-02-05 20:13:30 +0530

    added hosted urls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -129,7 +129,7 @@ function App() {
         data = { image };
       }
 
-      const response = await fetch('http://127.0.0.1:8000', {
+      const response = await fetch('https://ingredient-safety-analyzer.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
